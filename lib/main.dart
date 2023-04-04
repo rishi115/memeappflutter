02617cb8d964e15f9c9c2da5cd183546bbdcad3b
rishikesh devare <rishidevare051@gmail.com>
2023-04-05 00:00:34 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:mausam/view/MainScreen.dart';
+import 'package:mausam/view/mainScreen.dart';
 import 'package:mausam/view/splashScreen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool showSplash = true;
+  showSplashScreen(){
+    Future.delayed(Duration(seconds: 5),(){
+      setState(() {
+        showSplash = false;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    showSplashScreen();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'rishi App',
+      debugShowCheckedModeBanner: false,
+      title: 'UnSad',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MainScreen(),
+      home:  showSplash ? splashScreen() : MainScreen(),
     );
   }
 }
-

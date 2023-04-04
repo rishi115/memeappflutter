66615d9ed88e0b95f404ccdbfefc71e45b3a85bb
rishikesh devare <rishidevare051@gmail.com>
2023-04-04 21:28:22 +0530

    done

--- lib/view/mainScreen.dart
+++ lib/view/mainScreen.dart
@@ -1,55 +1,143 @@
-
 import 'package:flutter/material.dart';
 import 'package:mausam/controller/fetchmeme.dart';
+import 'package:mausam/controller/SaveMyData.dart';
+
 
 class MainScreen extends StatefulWidget {
-
-   MainScreen({Key? key}) : super(key: key);
+  MainScreen({Key? key}) : super(key: key);
 
   @override
   State<MainScreen> createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
-  String imgurl="https://images.unsplash.com/photo-1680004586355-51b867a28570?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=150&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MDU1NzI1MQ&ixlib=rb-4.0.3&q=80&w=500";
-  void UpdateImg() async {
-    String getImgUrl=await FetchMeme.fetchNewMeme();
+  String imgUrl = "";
+  int? memeNo;
+  int targetMeme = 100;
+  bool isLoading = true;
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    super.initState();
+
+    GetInitMemeNo();
+
+
+    UpdateImg();
+  }
+
+
+  GetInitMemeNo() async{
+    memeNo = await SaveMyData.fetchData() ?? 0;
+    if(memeNo!>100){
+      targetMeme = 500;
+    }else if(memeNo! > 500){
+      targetMeme  = 1000;
+    }
     setState(() {
-      imgurl=getImgUrl;
 
     });
   }
+  void UpdateImg() async{
+
+    String getImgUrl = await FetchMeme.fetchNewMeme();
+    // imgUrl = getImgUrl;
+    setState(() {
+      imgUrl = getImgUrl;
+      isLoading  = false;
+    });
+
+  }
+
+
+
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Column(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 120,
+            ),
+            Text(
+              "Meme #${memeNo.toString()}",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Target ${targetMeme} Memes",
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            isLoading ?
+            Container(
+                height : 400,
+                width : MediaQuery.of(context).size.width,
+                child : Center(
+                    child : SizedBox(
+                        height : 60,
+                        width : 60,
+                        child: CircularProgressIndicator())
+                )
+            )
+                :
 
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 180,),
-          Text("meme #21",style: TextStyle(fontSize: 30,fontWeight: FontWeight.w700),),
 
-          Text("Target 500 memes",style: TextStyle(fontSize: 15,)),
-          SizedBox(height: 60,),
-          Image.network(imgurl,),
-          SizedBox(height: 60,),
-          ElevatedButton(onPressed: (){
-            UpdateImg();
-          }, child: Container(
+            Image.network(
+                height : 400,
+                width : MediaQuery.of(context).size.width,
+                fit : BoxFit.fitHeight,
+                imgUrl),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () async{
+                  setState(() {
+                    isLoading = true;
 
-              height: 50,
-              width: 80,
-              child: Center(child: Text('More Fun')),
+                  });
+                  await SaveMyData.saveData(memeNo!+1);
+                  GetInitMemeNo();
 
-          )),
-          Spacer(),
-          Text('App created by ',style: TextStyle(fontSize: 15),),
-          Text('Rishi',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w700)),
-          SizedBox(height: 10,)
-        ],
+                  UpdateImg();
 
-      )
+                },
+                child: Container(
+                    height: 50,
+                    width: 150,
+                    child: Center(
+                        child: Text(
+                          "More Fun!!",
+                          style: TextStyle(fontSize: 20),
+                        )))),
+            Spacer(),
+            Text(
+              "APP CREATED BY",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "CODE WITH RISHI",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
